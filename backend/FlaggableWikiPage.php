b6d162d382adc7996f858d4266629c41a59785db
Krohn <krohnd@th-luebeck.de>
2019-07-29 14:06:05 +0200

    added hooks for 1.33

--- backend/FlaggableWikiPage.php
+++ backend/FlaggableWikiPage.php
@@ -483,6 +483,12 @@ class FlaggableWikiPage extends WikiPage {
 		);
 		# Update pending edit tracking table
 		self::updatePendingList( $this->getId(), $latest );
+		
+		# oncampus LOOP custom Hook.
+		# @author Dennis Krohn krohnden
+		Hooks::run( 'AfterStabilizeChange', array( $this->mTitle, $rev->getContent(), $srev->getUser() ) );
+		# /oncampus
+
 		return true;
 	}
 
@@ -499,6 +505,11 @@ class FlaggableWikiPage extends WikiPage {
 			[ 'fp_page_id' => $this->getId() ], __METHOD__ );
 		$dbw->delete( 'flaggedpage_pending',
 			[ 'fpp_page_id' => $this->getId() ], __METHOD__ );
+
+		# oncampus LOOP custom Hook.
+		# @author Dennis Krohn krohnden
+		Hooks::run( 'AfterClearStable', array( $this->mTitle ) );
+		# /oncampus
 	}
 
 	/**
